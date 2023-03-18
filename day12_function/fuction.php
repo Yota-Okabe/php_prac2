17d7dc1e4a6993afa1d137b64a78f1f7f392137e
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-03-18 15:33:46 +0900

    関数について、引数、戻り値

--- day12_function/fuction.php
+++ day12_function/fuction.php
@@ -1,2 +1,39 @@
 <?php
+    // 関数について学ぶ「自作関数」
+
+    // step1.定義する
+        // function 関数名(){繰り返し使いたい処理}
+    // step2.呼び出す
+        // 関数名();
+    
+    // 定義元
+    function hello(){
+        echo 'こんにちは' . '<br>';
+    }
+    // 呼び出し
+    hello();
+
+
+    // 引数
+    function greeting($test, $test1){
+        echo $test . $test1 . 'が引き渡されました' . '<br>';
+    }
+    greeting('おはようございます', 'ボンジュール');
+
+    function calc($fruit, $price){
+        echo '果物：' . $fruit . '<br>';
+        echo '税抜き価格：'  . $price. '円<br>';
+        echo '税込価格：' . $price * 1.1 . '円<br>';
+    }
+    calc('りんご',300);
+    calc('みかん',250);
+    calc('ぶどう',500);
+    calc('いちご',380);
+
+    // 戻り値
+    function calc2($num1, $num2){
+        return $num1 * $num2;
+    }
+    echo '合計' .  calc2(100, 1.1) + calc2(200, 1.1) . '円です';
+
 ?>
